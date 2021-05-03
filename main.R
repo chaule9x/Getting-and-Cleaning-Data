f529afc25c1636a8a328a5a9c384582c8cac6f20
chaule9x <56850085+chaule9x@users.noreply.github.com>
2021-05-03 16:18:50 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,3 +1,6 @@
+#-----------------------------------------------------------------------------------------
+#Chau Le
+#-----------------------------------------------------------------------------------------
 #download file and put to folder
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -7,3 +10,53 @@ unzip(zipfile="./data/Dataset.zip",exdir="./data")
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
+#-----------------------------------------------------------------------------------------
+#I.Merges the training and the test sets to create one data set
+#1.Concatenate the data tables by rows
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+#2.set names to variables
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+#3.Merge columns to get the data frame Data for all data
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+#-----------------------------------------------------------------------------------------
+#II.Extracts only the measurements on the mean and standard deviation for each measurement
+#1.Subset Name of Features by measurements on the mean and standard deviation
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+#2.Subset the data frame Data by seleted names of Features
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+#3.Check the structures of the data frame Data
+str(Data)
+#-----------------------------------------------------------------------------------------
+#III. Uses descriptive activity names to name the activities in the data set
+#1.Read descriptive activity names from “activity_labels.txt”
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+#2.facorize Variale activity in the data frame Data using descriptive activity names
+#3.check
+head(Data$activity,30)
+#-----------------------------------------------------------------------------------------
+#IV. Appropriately labels the data set with descriptive variable names
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+#-----------------------------------------------------------------------------------------
+#V.Creates a second,independent tidy data set and ouput it
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+#Writing second tidy data set in txt file
+write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
