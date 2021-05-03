918c74284594bdddead2c8a613490de210dbd3f4
chaule9x <56850085+chaule9x@users.noreply.github.com>
2021-05-03 16:10:17 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,3 +1,9 @@
+#download file and put to folder
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+#unzip folder and file
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
